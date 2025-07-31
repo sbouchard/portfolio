acb7513995f59e84520e4680e8e3fb631fea3c78
Sylvain Bouchard <sylvain@waizulabs.com>
2025-07-31 18:36:39 -0400

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,103 @@
+export const siteConfig = {
+  name: "Sylvain Bouchard",
+  title: "Senior iOS Software Engineer",
+  description: "Portfolio website of Waizu Labs",
+  accentColor: "#793CE3",
+  social: {
+    email: "sylvain@waizulabs.com",
+    linkedin: "www.linkedin.com/in/sylvainbouchard",
+    github: "https://github.com/sbouchard",
+  },
+  aboutMe:
+    "I'm a seasoned software engineer with over two decades of experience in the tech industry. I began my career in 2000, working across various platforms and technologies. Since 2007, when the first iPhone launched, I’ve focused exclusively on iOS development—building elegant, efficient, and scalable mobile applications for Apple’s ecosystem. Over the years, I've contributed to projects ranging from early App Store launches to modern SwiftUI-based applications, consistently staying current with Apple’s evolving frameworks and design philosophies. Whether working solo or leading a team, I take pride in writing clean code, crafting intuitive user experiences, and shipping reliable products. I'm passionate about mobile technology, and I'm always exploring ways to push the boundaries of what's possible on iOS. ",
+  skills: ["Swift", "Objective-C", "Xcode", "SwiftUI", "UIKit", "Git", "CocoaPods", "SPM", "Fastlane", "TestFlight"],
+  projects: [
+    {
+      name: "SwiftConnect",
+      description:
+        "SwiftConnect Mobile enables users to replace physical access cards with digital credentials on iPhone or Apple Watch, allowing seamless entrance to buildings, offices, and amenity spaces via Apple Wallet integration.",
+      link: "https://apps.apple.com/us/app/swiftconnect-mobile/id1585852507",
+      skills: ["Swift", "UIKit", "Cocoa Pods", "Swift Package Manager", "PassKit", "Firebase"],
+    },
+    {
+      name: "Disnat",
+      description:
+        "The Disnat mobile app is the companion to Desjardins Online Brokerage (Disnat), offering independent investors the ability to manage portfolios and trade efficiently via smartphone or tablet. ",
+      link: "https://apps.apple.com/ca/app/disnat/id1053208549",
+      skills: ["Swift", "UIKit"],
+    },
+    {
+      name: "Desjardins Insurance",
+      description:
+        "The Desjardins Insurance Home‑Auto app lets policyholders manage car and home insurance conveniently from their iPhone or iPad and access key services anytime. Integrated a telematics program that monitors driving patterns—speed, braking, trip timing—to tailor insurance premiums",
+      link: "https://apps.apple.com/ca/app/desjardins-insurance/id958825069",
+      skills: ["Swift", "Objective-C", "UIKit"],
+    },
+    {
+      name: "Desjardins Banking App",
+      description:
+        "Desjardins’ Mobile Services app (also called AccèsD Mobile) enables secure, fast, and flexible management of your financial life using iPhone or iPad",
+      link: "https://apps.apple.com/ca/app/desjardins-mobile-services/id386636953",
+      skills: ["Swift", "Objective-C", "UIKit"],
+    },
+    {
+      name: "LiveBarn",
+      description:
+        "LiveBarn delivers live and on-demand streaming of amateur and youth sports—the app supports ice hockey, basketball, volleyball, soccer, baseball, lacrosse, and more at over 3,000 venues across North America",
+      link: "https://apps.apple.com/ca/app/livebarn/id1434353736",
+      skills: ["Swift", "UIKit", "AVKit"],
+    },
+    {
+      name: "IDEXX VetConnect PLUS",
+      description:
+        "VetConnect PLUS is the mobile extension of IDEXX’s diagnostic platform. It lets veterinarians and clinic staff view, interpret, and act upon IDEXX lab results at any time right from their iPhone or iPad ",
+      link: "https://apps.apple.com/us/app/idexx-vetconnect-plus/id791228999",
+      skills: ["Objective-C", "C", "Cocoa touch", "GIT", "Shinobi Chart", "Cocoa Pods", "Core Data", "Fabric", "Crashlytics"],
+    },
+
+
+
+  ],
+  experience: [
+    {
+      company: "SwiftConnect",
+      title: "Manager / Tech Lead / Senior Software Engineer",
+      dateRange: "Jan 2022 - Present",
+      bullets: [
+      "Lead the design, architecture, and development of mobile applications, ensuring adherence to best practices, coding standards, and scalability.",
+      "Provide technical guidance and mentorship to the mobile development team, fostering a culture of learning and innovation.",
+      "Stay abreast of industry trends, emerging technologies, and best practices in mobile development, and incorporate them into the team's processes and projects.",
+      "Collaborate with cross-functional teams, including product managers, designers, and backend developers, to define project requirements, timelines, and deliverables.",
+      "Build and nurture a high-performing mobile development team, fostering a culture of collaboration, accountability, and continuous improvement.",
+      "Act as a liaison between the mobile development team and other departments, facilitating communication, resolving conflicts, and promoting a collaborative working environment."
+      ],
+    },
+    {
+      company: "Desjardins / Disnat",
+      title: "Senior iOS Developer",
+      dateRange: "Jun 2016 - Present",
+      bullets: [
+        "Collaborated with the in-house mobile team to enhance and maintain the Disnat/VMD iOS trading app, focusing on performance improvements, bug fixes, and UI refinements.",
+        "Contributed to key features such as real-time market data display, secure login flows, and trade execution workflows.",
+        "Ensured compliance with accessibility standards and App Store guidelines while supporting a secure and responsive user experience for thousands of active investors."
+      ],
+    }
+  ],
+  education: [
+    {
+      school: "Sherbrooke University",
+      degree: "Bachelor in Computer Science",
+      dateRange: "1995 - 1999",
+      achievements: [
+      ],
+    },
+    {
+      school: "Trois-Rivieres College",
+      degree: "Diploma of College Studies in Computer Science",
+      dateRange: "1995 - 1995",
+      achievements: [
+        
+      ],
+    },
+  ],
+};
